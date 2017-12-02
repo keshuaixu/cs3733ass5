2ba8e61b49bccbad5b9af65c876f7b3ef72ffc98
Keshuai Xu <i@xks.im>
2017-12-02 16:07:17 -0500

    fuck me

--- src/controllers/ObserverController.java
+++ src/controllers/ObserverController.java
@@ -8,8 +8,8 @@ import javafx.scene.text.Text;
 import models.Time;
 import observers.*;
 
-import java.time.LocalDate;
-import java.util.Calendar;
+import java.time.*;
+
 
 public class ObserverController {
 
@@ -24,23 +24,31 @@ public class ObserverController {
     @FXML
     Label labelJan1970EST, labelBeforeDecade, labelPST;
 
-    Time time;
+    private Time time = new models.Time();
 
     /**
      * Controller object which will initialize what kind of observers are listening to the subject (ie the Eastern Time
      * Zone) and notify observers of updates when something changes in the EST fields.
      */
     @FXML
-    public void initialize(){
+    public void initialize() {
+        time.attachObserver(new ObsBeforeDecade(time, labelBeforeDecade));
+        time.attachObserver(new ObsJan1970EST(time, labelJan1970EST));
+        time.attachObserver(new ObsPacificStandard(time, labelPST));
     }
 
     // Hint: When using Calendar.set(), month starts with 0
+
     /**
      * Action function for the textfields and datepicker associated with the Eastern Time Zone.
      * Will set the value of the subject inherently notifying all of its observers.
      * Calendar.getInstance() and set() is usefull here
-     *
      */
-    public void updateTime() throws NumberFormatException{
+    public void updateTime() throws NumberFormatException {
+        LocalDate date = ESTDate.getValue();
+        LocalTime localTime = LocalTime.of(Integer.parseInt(txtESTHours.getText()), Integer.parseInt(txtESTMinutes.getText()), Integer.parseInt(txtESTSeconds.getText()));
+        LocalDateTime localDateTime = LocalDateTime.of(date, localTime);
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of("EST"));
+        time.setValue(zonedDateTime);
     }
 }
--- src/models/Time.java
+++ src/models/Time.java
@@ -3,6 +3,7 @@ package models;
 import observers.AbsTimeZoneObserver;
 
 import java.lang.reflect.Array;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Observer;
@@ -13,33 +14,42 @@ import java.util.Observer;
  */
 public class Time {
 
-    Calendar calendar;
     ArrayList<AbsTimeZoneObserver> observers;
+    private ZonedDateTime zonedDateTime;
 
-    public Time () {
+    public Time() {
         // Don't forget to create an instance of Calendar.
         // Note Calendar cannot be initialized with new Calendar(), and Calendar.getInstance() must be used instead
     }
 
     /**
      * Accessor for the value field
+     *
      * @return The current value of the time
      */
-    public Calendar getValue(){
+    public Calendar getValue() {
         return null;
     }
 
     /**
      * Setter for the value field
+     *
      * @param value New value to propagate to clients
      */
-    public void setValue(Calendar value){ }
+    @Deprecated
+    public void setValue(Calendar value) {
+    }
+
+    public void setValue(ZonedDateTime value) {
+        this.zonedDateTime = value;
+    }
 
     /**
      * Adds a new observer client to the list of observers for this value
+     *
      * @param observer An instance of an observer
      */
-    public void attachObserver(AbsTimeZoneObserver observer){
+    public void attachObserver(AbsTimeZoneObserver observer) {
         this.observers.add(observer);
     }
 
@@ -47,5 +57,6 @@ public class Time {
      * Calls the update function to tell each observer this time's value
      * has changed so they need to update.
      */
-    public void notifyObservers(){ }
+    public void notifyObservers() {
+    }
 }
