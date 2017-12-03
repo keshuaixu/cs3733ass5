96bdd5194830860293b1b2a5092274ab12d9da0e
Keshuai Xu <i@xks.im>
2017-12-02 23:11:37 -0500

    fucked before decade

--- src/observers/ObsBeforeDecade.java
+++ src/observers/ObsBeforeDecade.java
@@ -3,6 +3,9 @@ package observers;
 import javafx.scene.control.Label;
 import models.Time;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
@@ -15,14 +18,19 @@ public class ObsBeforeDecade extends AbsTimeZoneObserver {
     /**
      * Constructor; Should set the time zone for the calendar object
      *
-     * @param time Time object we are listening to
+     * @param time  Time object we are listening to
      * @param label Label to display this observers information
      */
-    public ObsBeforeDecade (Time time, Label label) {
+    public ObsBeforeDecade(Time time, Label label) {
         super(time, label);
     }
 
     @Override
     public void update() {
+        LocalDate localDate = this.time.getZonedDateTime().toLocalDate();
+        int year = (int) Math.floor(localDate.getYear()/10.) * 10 + 9;
+        LocalDate endOfDecade = LocalDate.of(year, 12, 31);
+        long daysBeforeEnd = ChronoUnit.DAYS.between(localDate, endOfDecade);
+        this.label.setText(String.format("%d BD", daysBeforeEnd));
     }
 }
